a1260a0bd6e8103ad36400c24d8ad3ad786cc794
Omar Shebl <omarshebl36@outlook.com>
2023-07-16 15:58:25 +0300

    feat: handle click of the dropdown and its options

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 type Option = {
   label: string;
   value: string;
@@ -8,9 +10,29 @@ interface Props {
 }
 
 function Dropdown({ options }: Props) {
+  const [isOpen, setIsOpen] = useState(false);
+  const [selectedOption, SetselectedOption] = useState("Select...");
+
+  const handleClick = () => {
+    setIsOpen((currentValue) => !currentValue);
+  };
+
+  const handleOptionClick = (selectedOption: Option) => {
+    SetselectedOption(selectedOption.label);
+    setIsOpen(false);
+  };
+
+  const renderOptions = options.map((option) => {
+    return (
+      <div onClick={() => handleOptionClick(option)} key={option.value}>
+        {option.label}
+      </div>
+    );
+  });
   return (
     <div>
-      <h1>Dropdown</h1>
+      <div onClick={handleClick}>{selectedOption}</div>
+      {isOpen && <div>{renderOptions}</div>}
     </div>
   );
 }
