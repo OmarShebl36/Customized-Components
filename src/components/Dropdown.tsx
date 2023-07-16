f2ad222470236b65309f3ea32f42603b372fa84b
Omar Shebl <omarshebl36@outlook.com>
2023-07-16 16:48:41 +0300

    feat: create a Panel component

--- src/components/Dropdown.tsx
+++ src/components/Dropdown.tsx
@@ -1,4 +1,7 @@
 import { useState } from "react";
+import "../index.css";
+import { GoChevronDown } from "react-icons/go";
+
 
 export type Option = {
   label: string;
@@ -7,8 +10,8 @@ export type Option = {
 
 interface Props {
   options: Option[];
-	onChange: (value: Option | null) => void;
-	value: Option | null;
+  onChange: (value: Option | null) => void;
+  value: Option | null;
 }
 
 function Dropdown({ options, value, onChange }: Props) {
@@ -19,21 +22,35 @@ function Dropdown({ options, value, onChange }: Props) {
   };
 
   const handleOptionClick = (selectedOption: Option | null) => {
-		onChange(selectedOption);
+    onChange(selectedOption);
     setIsOpen(false);
   };
 
   const renderOptions = options.map((option) => {
     return (
-      <div onClick={() => handleOptionClick(option)} key={option.value}>
+      <div
+        className="hover:bg-sky-100 rounded cursor-pointer p-1"
+        onClick={() => handleOptionClick(option)}
+        key={option.value}
+      >
         {option.label}
       </div>
     );
   });
   return (
-    <div>
-      <div onClick={handleClick}>{value?.label || 'Select...'}</div>
-      {isOpen && <div>{renderOptions}</div>}
+    <div className="w-48 relative">
+      <div
+        className="flex justify-between items-center cursor-pointer"
+        onClick={handleClick}
+      >
+        {value?.label || "Select..."}
+				<GoChevronDown className="text-lg"/>
+      </div>
+      {isOpen && (
+        <div className="absolute top-full">
+          {renderOptions}
+        </div>
+      )}
     </div>
   );
 }
