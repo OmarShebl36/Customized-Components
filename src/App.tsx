00e3c8e5c3d4f9d1eb6ce49c6d515a1bce0cbd09
Omar Shebl <omarshebl36@outlook.com>
2023-08-01 13:11:13 +0300

    feat: create a Sidebar component and use it in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
-import Link from "./components/Link";
 import Route from "./components/Route";
+import Sidebar from "./components/Sidebar";
 import AccordionPage from "./pages/AccordionPage";
+import ButtonPage from "./pages/ButtonPage";
 import DropdownPage from "./pages/DropdownPage";
 
 function App() {
   return (
     <>
-      <Link to="/accordion">Go to accordion</Link>
-      <Link to="/dropdown">Go to dropdown</Link>
+      <Sidebar />
       <Route path="/accordion">
         <AccordionPage />
       </Route>
       <Route path="/dropdown">
         <DropdownPage />
+      </Route>
+      <Route path="/buttons">
+        <ButtonPage />
       </Route>
     </>
   );
