dac3f3c65ba7016b4a9028b7af11bcf1aa8e8f86
Omar Shebl <omarshebl36@outlook.com>
2023-08-01 12:05:38 +0300

    chore: remove dropdown page content from the app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,5 @@
-import { useState } from "react";
-import Dropdown, { Option } from "./components/Dropdown";
-
 function App() {
-  const [selection, setSelection] = useState<Option | null>(null);
-
-  const handleSelect = (selectedValue: Option | null) => {
-    setSelection(selectedValue);
-  };
-
-  const options = [
-    { label: 'Red', value: 'Red' },
-    { label: 'Green', value: 'Green`' },
-    { label: 'Blue', value: 'Blue' },
-  ]
-  return <Dropdown options={options} value={selection} onChange={handleSelect} />;
+  return <></>;
 }
 
 export default App;
