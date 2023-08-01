62692c8ff1b95cfc6077bccefd102e31dab31f68
Omar Shebl <omarshebl36@outlook.com>
2023-08-01 18:07:18 +0300

    feat: add modal component using portals

--- src/pages/ModalPage.tsx
+++ src/pages/ModalPage.tsx
@@ -1,9 +1,33 @@
+import { useState } from "react";
+import Button from "../components/Button";
 import Modal from "../components/Modal";
 
 function ModalPage() {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleClick = () => {
+    setShowModal(true);
+  };
+
+  const handleClose = () => {
+    setShowModal(false);
+  };
+
+  const actionBar = <div>
+    <Button onClick={handleClose} primary>I Accept</Button>
+  </div>;
+  const modal = (
+    <Modal onClose={handleClose} actionBar={actionBar}>
+      <p>Here is an important agreement for you to accept.</p>
+    </Modal>
+  );
+
   return (
-    <div>
-      <Modal />
+    <div className='relative'>
+      <Button primary onClick={handleClick}>
+        Open Modal
+      </Button>
+      {showModal && modal}
     </div>
   );
 }
