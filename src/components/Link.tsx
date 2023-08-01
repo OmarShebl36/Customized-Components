c4a72d08d525f0a178efcca596b898ed33a7a917
Omar Shebl <omarshebl36@outlook.com>
2023-08-01 13:02:22 +0300

    feat: create useNavigation hook and use it

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -1,13 +1,16 @@
-import { ReactNode, useContext } from "react";
-import NavigationContext from "../context/navigation";
+import classNames from "classnames";
+import { ReactNode } from "react";
+import useNavigation from "../hooks/use-navigation";
 
 interface Props {
   to: string;
   children: ReactNode;
+  additionalClassNames?: string;
 }
 
-function Link({ to, children }: Props) {
-  const { navigate } = useContext(NavigationContext);
+function Link({ to, children, additionalClassNames }: Props) {
+  const classes = classNames("text-blue-500", additionalClassNames);
+  const { navigate } = useNavigation();
   const handleClick = (event: React.MouseEvent) => {
     if (event.metaKey || event.ctrlKey) {
       return;
@@ -18,7 +21,7 @@ function Link({ to, children }: Props) {
   };
 
   return (
-    <a href={to} onClick={handleClick}>
+    <a className={classes} href={to} onClick={handleClick}>
       {children}
     </a>
   );
--- src/components/Route.tsx
+++ src/components/Route.tsx
@@ -1,5 +1,4 @@
-import { useContext } from "react";
-import NavigationContext from "../context/navigation";
+import useNavigation from "../hooks/use-navigation";
 
 interface Props {
   path: string;
@@ -7,7 +6,7 @@ interface Props {
 }
 
 function Route({ path, children }: Props) {
-  const { currentPath } = useContext(NavigationContext);
+  const { currentPath } = useNavigation();
   if (path === currentPath) return children;
   return null;
 }
