88b9ee491cdb92e7bbf1c8313d7829442c107f6f
Omar Shebl <omarshebl36@outlook.com>
2023-08-01 12:26:22 +0300

    rfr: use the app component to test the Link component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,10 @@
+import Link from "./components/Link";
+
 function App() {
-  return <></>;
+  return <>
+  <Link to="/accordion">Go to accordion</Link>
+  <Link to="/dropdown">Go to dropdown</Link>
+  </>;
 }
 
 export default App;
