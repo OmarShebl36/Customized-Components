fc898f5d3db996eb84c611399d893dbccda4c4d4
Omar Shebl <omarshebl36@outlook.com>
2023-08-01 12:06:28 +0300

    feat: create a context provider and pass it to index.tsx

---
+++ src/context/navigation.tsx
@@ -0,0 +1,31 @@
+import { ReactNode, createContext, useEffect, useState } from "react";
+
+const NavigationContext = createContext({});
+
+interface NavigationProps {
+  children: ReactNode;
+}
+
+function NavigationProvider({ children }: NavigationProps) {
+  const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+  useEffect(() => {
+    const handler = () => {
+      setCurrentPath(window.location.pathname);
+    };
+
+    window.addEventListener("popstate", handler);
+    return () => {
+      window.removeEventListener("popstate", handler);
+    };
+  }, []);
+
+  return (
+    <NavigationContext.Provider value={{}}>
+      {children}
+    </NavigationContext.Provider>
+  );
+}
+
+export { NavigationProvider };
+export default NavigationContext;
