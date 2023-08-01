8e9589d8c21fe02f6e6d7a5f7f325b516e05200a
Omar Shebl <omarshebl36@outlook.com>
2023-08-01 12:52:13 +0300

    feat: add new tap feature to the Link comp

--- src/components/Link.tsx
+++ src/components/Link.tsx
@@ -9,11 +9,19 @@ interface Props {
 function Link({ to, children }: Props) {
   const { navigate } = useContext(NavigationContext);
   const handleClick = (event: React.MouseEvent) => {
+    if (event.metaKey || event.ctrlKey) {
+      return;
+    }
     event.preventDefault();
+
     navigate(to);
   };
-  // eslint-disable-next-line jsx-a11y/anchor-is-valid
-  return <a onClick={handleClick}>{children}</a>;
+
+  return (
+    <a href={to} onClick={handleClick}>
+      {children}
+    </a>
+  );
 }
 
 export default Link;
